ff6ab92a2fc420407d1bf9a72ff044ef3f43c97d
ashjian <68432170+ashjian@users.noreply.github.com>
2022-01-06 00:15:05 +0800

    Add files via upload

---
+++ video.js
@@ -0,0 +1,15 @@
+var Hls = window.Hls
+// var url = 'https://cdn.jsdelivr.net/gh/ashjian/v3937/3938/playlist.m3u8'
+if (Hls.isSupported()) {
+  var hls = new Hls()
+  hls.loadSource(video.src)
+  hls.attachMedia(video)
+  hls.on(Hls.Events.MANIFEST_PARSED, function () {
+    // video.play()
+  })
+} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+  // video.src = url
+  video.addEventListener('canplay', function () {
+    // video.play()
+  })
+}
