fa004dcc2ad460b9b005b8fb99a90d2c9e0bbb65
ashjian <68432170+ashjian@users.noreply.github.com>
2022-01-06 10:20:25 +0800

    Update video.js

--- video.js
+++ video.js
@@ -1,9 +1,9 @@
 var Hls = window.Hls
-var video = document.getElementById("video")
-// var url = 'https://cdn.jsdelivr.net/gh/ashjian/v3937/3938/playlist.m3u8'
+// var video = document.getElementById("video")
+var url = 'https://cdn.jsdelivr.net/gh/ashjian/v3937/3938/playlist.m3u8'
 if (Hls.isSupported()) {
   var hls = new Hls()
-  hls.loadSource(video.src)
+  hls.loadSource(url)
   hls.attachMedia(video)
   hls.on(Hls.Events.MANIFEST_PARSED, function () {
     // video.play()
