ff4caf7a5010b30ad9735787ad5b5a9eadf41751
ashjian <68432170+ashjian@users.noreply.github.com>
2022-01-06 11:08:41 +0800

    Update video.js

--- video.js
+++ video.js
@@ -1,16 +1,22 @@
-var Hls = window.Hls
-// var video = document.getElementById("video")
-var url = 'https://cdn.jsdelivr.net/gh/ashjian/v3937/3938/playlist.m3u8'
-if (Hls.isSupported()) {
-  var hls = new Hls()
-  hls.loadSource(url)
-  hls.attachMedia(video)
-  hls.on(Hls.Events.MANIFEST_PARSED, function () {
-    video.play()
-  })
-} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-  // video.src = url
-  video.addEventListener('canplay', function () {
-    video.play()
-  })
+document.addEventListener("DOMContentLoaded", function () {
+  playm3u8();
+}, false);
+
+function playm3u8() {
+  var Hls = window.Hls
+  // var video = document.getElementById("video")
+  var url = 'https://cdn.jsdelivr.net/gh/ashjian/v3937/3938/playlist.m3u8'
+  if (Hls.isSupported()) {
+    var hls = new Hls()
+    hls.loadSource(url)
+    hls.attachMedia(video)
+    hls.on(Hls.Events.MANIFEST_PARSED, function () {
+      video.play()
+    })
+  } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    // video.src = url
+    video.addEventListener('canplay', function () {
+      video.play()
+    })
+  }
 }
